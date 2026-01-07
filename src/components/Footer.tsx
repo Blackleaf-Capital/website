e27b70b4d49b160e9a91a41cb3afd0358b71c69f
Darryl Mbae <darrylmbae01@gmail.com>
2026-01-07 18:53:37 +0300

    feat:Updating links and Landing Page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom"
 import background from "../assets/images/background.jpg"
 import { FaLinkedinIn, FaInstagram, FaRegCopyright } from "react-icons/fa6"
 import { MdOutlineEmail } from "react-icons/md"
-import image from "../assets/images/logo.png"
 
 const Footer = () => {
     const scrollToTop = () => {
@@ -23,7 +22,6 @@ const Footer = () => {
                             className="w-full h-full object-cover"
                         />
                     </div>
-
                     <div className="absolute inset-0 bg-primary/90"></div>
 
                     <div className="relative w-[95%] my-auto z-10 h-full flex flex-col lg:flex-row items-center justify-between px-8 md:px-12 lg:px-16 py-8 gap-6">
@@ -64,10 +62,13 @@ const Footer = () => {
                 <div className="w-[80%] lg:w-[90%] mx-auto grid py-20">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[45%_55%] gap-8">
                         <div className="flex flex-col">
-                            <div className="h-[7.5vh] mb-5">
-                                <img src={image} alt="Blackleaf Capital Logo" className='h-[150%] w-auto' />
+                            <div className="mb-5 flex flex-row items-center gap-4">
+                                <div className="font-heading text-white text-4xl">BC</div>
+                                <div className="w-px h-[30px] bg-white/40"></div>
+                                <div className="font-primary text-white/80">Blackleaf <br />Capital</div>
+
                             </div>
-                            <p className="w-[90%] my-5 md:mt-8 text-white/80">Bridging the gap between potential and opportunity</p>
+                            <p className="w-[90%] my-5 md:mt-8 text-white/90">Bridging the gap between potential and opportunity</p>
                             <div className="flex flex-row gap-2 mt-4">
                                 <a
                                     href="https://www.linkedin.com/company/blackleaf-capital/"
@@ -117,13 +118,13 @@ const Footer = () => {
                                 <div className="text-[20px] font-bold text-white mb-5">Resources</div>
                                 <ul className="flex flex-col gap-3">
                                     <li className="cursor-pointer text-white/80 hover:text-white transition-all">
-                                        <Link to="/" onClick={scrollToTop}>About</Link>
+                                        <Link to="/faqs">FAQs</Link>
                                     </li>
                                     <li className="cursor-pointer text-white/80 hover:text-white transition-all">
-                                        <Link to="/team" onClick={scrollToTop}>Portfolio</Link>
+                                        <Link to="/events">Upcoming Events</Link>
                                     </li>
                                     <li className="cursor-pointer text-white/80 hover:text-white transition-all">
-                                        <Link to="/events" onClick={scrollToTop}>News</Link>
+                                        <Link to="/events">Gallery</Link>
                                     </li>
                                 </ul>
                             </div>
@@ -133,7 +134,9 @@ const Footer = () => {
                                     <li className="cursor-pointer text-white/80 hover:text-white transition-all">
                                         <Link to="/sponsors" onClick={scrollToTop}>Sponsors & Partners</Link>
                                     </li>
-                                    <li className="cursor-pointer text-white/80 hover:text-white transition-all">Investments</li>
+                                    <li className="cursor-pointer text-white/80 hover:text-white transition-all">
+                                    <Link to="/team" onClick={scrollToTop}>Alumini</Link>
+                                    </li>
                                     <li className="cursor-pointer text-white/80 hover:text-white transition-all">
                                         <Link to="/join" onClick={scrollToTop}>Join Us</Link>
                                     </li>
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,30 +1,42 @@
 import { MdOutlineArrowRightAlt } from "react-icons/md";
 import { Link } from "react-router-dom"
+import background from "../assets/images/network.jpeg"
 
 function LandingPage() {
     const scrollToTop = () => {
         window.scrollTo({
-          top: 0,
-          behavior: 'smooth'
+            top: 0,
+            behavior: 'smooth'
         });
-      };
+    };
 
     return (
         <div className="space-y-8  ">
-            <div className='w-[90%] mt-[4vh] mx-auto h-[80vh] bg-black rounded-2xl flex flex-col items-center justify-center text-white text-2xl'>
-                <h1 className="font-primary text-2xl lg:text-4xl w-[80%] lg:w-[50%] text-center">
-                Bridging the gap between potential and Success
-                </h1>
-                <p className="text-[16px] w-[80%] lg:w-[40%] my-5 text-center">We help students move from ambition to achievement through real-world experience and industry access.</p>
-                <Link
-                    to="/sponsors"
-                    onClick={scrollToTop}
-                    className="rounded-4xl bg-primary text-white px-8 py-3 flex flex-row items-center gap-4 cursor-pointer transition-all text-[16px] ease-in-out hover:translate-x-2 font-secondary flex-start"
-                >
-                    The Blackleaf Way
-                    <MdOutlineArrowRightAlt />
-
-                </Link>
+            <div className='relative w-[90%] mt-[4vh] mx-auto h-[80vh] rounded-2xl flex flex-col items-center justify-center text-white text-2xl overflow-hidden'>
+                <div className="absolute inset-0">
+                    <img
+                        src={background}
+                        alt="CTA Background"
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="absolute inset-0 bg-black/50"></div>
+                <div className="relative w-full h-full flex flex-col items-center justify-center">
+                    <h1 className="font-primary text-2xl lg:text-4xl w-[80%] lg:w-[50%] text-center">
+                        Bridging the gap between potential and Success
+                    </h1>
+                    <p className="text-[16px] w-[80%] lg:w-[40%] my-5 text-center">
+                        We help students move from ambition to achievement through real-world experience and industry access.
+                    </p>
+                    <Link
+                        to="/sponsors"
+                        onClick={scrollToTop}
+                        className="rounded-4xl bg-primary text-white px-8 py-3 flex flex-row items-center gap-4 cursor-pointer transition-all text-[16px] ease-in-out hover:translate-x-2 font-secondary"
+                    >
+                        The Blackleaf Way
+                        <MdOutlineArrowRightAlt />
+                    </Link>
+                </div>
             </div>
         </div>
     )
